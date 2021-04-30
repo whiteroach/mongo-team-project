fca787274ba7fbef1a84b0310f56e1687658a39b
camposCode <info@josecampos.io>
2021-04-30 11:27:05 +0200

    routes created

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ app.set('view engine', 'hbs');
 
 const PORT = process.env.PORT || 8080;
 
+
+//********BODY PARSER****** */
+app.use(express.json());
+
+//**********ROUTES********* */
+app.use('/', require('./routes/auth'));
+
 app.listen(PORT, ()=>{`Listen to PORT ${PORT}`})
--- routes/auth.js
+++ routes/auth.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 const { createUser, loginUser } = require('../controllers/auth');
+
+router.post('/', loginUser);
+
+router.post('/newUser', createUser);
+
+module.exports = router;
