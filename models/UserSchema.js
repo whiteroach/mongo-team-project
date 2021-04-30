e9ea4ad920aabf3bfc65be71379eb696daf5b73a
camposCode <info@josecampos.io>
2021-04-30 10:23:21 +0200

    user schema done

---
+++ models/UserSchema.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+    status:{
+        admin: false
+    },
+    name: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    surname:{
+        type: String,
+        require: true,
+        trim: true
+    },
+    email:{
+        type: String,
+        require: true,
+        trim: true
+    },
+    password:{
+        type: String,
+        require: true,
+        trim: true
+    },
+    country:{
+        type: String,
+        trim: true
+    },
+    address:{
+        type: String,
+        trim: true
+    },
+});
+
+module.exports = model('User', UserSchema);
