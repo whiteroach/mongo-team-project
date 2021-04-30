e403eb87a7303e9906f462aa8adad049e9e2feb2
whiteroach <pietro.simcic@gmail.com>
2021-04-30 11:19:25 +0200

    added database connection in app.js

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+const mongoose = require('mongoose');
 
+//settings
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
+app.use(express.urlencoded({extended:false}))
 
+
+//database
+const DB_NAME = process.env.DB_NAME;
+const DB_LINK = process.env.DB_LINK + DB_NAME;
+
+mongoose.connect(DB_LINK, {useUnifiedTopology:true, useNewUrlParser:true})
+.then(()=>{console.log('Mongoose found his way to the database...')})
+.catch(err =>{console.log(err)})
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, ()=>{`Listen to PORT ${PORT}`})
