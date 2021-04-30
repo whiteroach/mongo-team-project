fe897a97a4d4f7d9a2a459a13c09092028730090
ashikkhandani <mdashikahammed@gmail.com>
2021-04-30 15:24:23 +0200

    get updated file

--- routes/product.js
+++ routes/product.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/product");
 
-<<<<<<< HEAD
-
-router.get('/', productController.displayProduct)
-=======
 router.get("/product", productController.displayProduct);
->>>>>>> 79d058dd933f42c0a63dcce082277cb27b67dba2
 
 module.exports = router;
