526a96c1046561facf40db0b82a4b4c8887c5b56
ashikkhandani <mdashikahammed@gmail.com>
2021-04-30 15:19:56 +0200

    updating form style

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,42 +1,5 @@
 const { response } = require("express");
 const User = require("../models/UserSchema");
-<<<<<<< HEAD
-const { use } = require("../routes/auth");
-
-const createUser = async (req, res = response) => {
- 
-
- const { email, password } = req.body;
-  try {
-    
-    let user = await User.findOne({ email });
-    
-
-    //******VERIFY USER******* */
-    if(user){
-        return res.status(400).json({
-             ok: false,
-             message: "email exists in our database"
-        });
-    }
-
-    user = new User(req.body);
-    await user.save();
-
-    res.status(201).json({
-      ok: true,
-      uid: user.id,
-      name: user.name
-    });
-  } catch (error) {
-    res.status(500).json({
-      ok: false,
-      msg: "unknown error",
-    });
-  }
-};
-
-=======
 
 const createUser = async (req, res = response) => {
   try {
@@ -55,7 +18,6 @@ const createUser = async (req, res = response) => {
   }
 };
 
->>>>>>> e71955660f206dbcc08bd9453c30ebc301528094
 const loginUser = (req, res = response) => {
   const { email, password } = req.body;
   res.json({
@@ -69,8 +31,4 @@ const loginUser = (req, res = response) => {
 module.exports = {
   createUser,
   loginUser,
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> e71955660f206dbcc08bd9453c30ebc301528094
